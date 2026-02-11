dd6bf563e494df0b47e3f56f16c16a6d342b771e
Arunava Chakraborty <144585145+Arunava-chakrabor@users.noreply.github.com>
2026-02-11 22:29:54 +0530

    Initial commit

---
+++ tuple.py
@@ -0,0 +1,26 @@
+marks=(3,4,6,5,8)
+print(type(marks))
+print(marks[0])
+
+
+#wap to ask the user to enter names of their 3 fav movies & store them in a list.
+# movies=[]
+# movies.append(input("Enter the name of your 1st fav movie: "))
+# movies.append(input("Enter the name of your 2nd fav movie: "))
+# movies.append(input("Enter the name of your 3rd fav movie: "))
+
+# print(movies)
+
+
+
+#wap to check if a list contains a palindrome of elements.(hint:use copy()method)    ex= [1,2,3,2,1]
+
+list1=[1,2,1]
+list2=[1,2,3]
+copy_list1=list1.copy()
+copy_list1.reverse()
+
+if(copy_list1==list1):
+    print("list1 is a palindrome")
+else:
+    print("list1 is not a palindrome")
