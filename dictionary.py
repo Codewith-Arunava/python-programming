403e3cc2467adcd560b71e6ad22564601a48a96e
Arunava Chakraborty <144585145+Arunava-chakrabor@users.noreply.github.com>
2026-02-19 13:56:37 +0530

    dict

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,36 @@
-info={
-    "key": "value",
+# info={
+#     "key": "value",
+#     "name": "John Doe",
+#     "learning": "Python",
+#     "topics": ["Data Structures", "OOP", "Web Development"]
+# }
+# print(info)
+# print(type(info))
+# print(info["name"])
+# info["name"] = "arunava"
+# print(info)
+
+
+#NESTED DICTIONARY
+student={
     "name": "John Doe",
-    "learning": "Python",
-    "topics": ["Data Structures", "OOP", "Web Development"]
+    "age": 20,
+    "courses": {
+        "course1": "Data Structures",
+        "course2": "OOP",
+        "course3": "Web Development"
+    }
 }
-print(info)
+print(student)
+print(student["courses"])
+print(student["courses"]["course2"])
+
+print(list(student.keys()))
+print(len(student))
+
+print(student.values())
+
+pairs=list(student.items())
+print(pairs[2])
+
+print(student.get("name"))
