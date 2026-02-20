4824aaab9e3010bf52ceeeaffe00be19902ceae8
Arunava Chakraborty <144585145+Arunava-chakrabor@users.noreply.github.com>
2026-02-20 23:29:13 +0530

    tuple

--- set.py
+++ set.py
@@ -1,11 +1,25 @@
-collection={1,2,3,4}
-print(collection)
-print(type(collection))
-print(len(collection))
+# collection={1,2,3,4}
+# print(collection)
+# print(type(collection))
+# print(len(collection))
 
-collection.add(5)
-print(collection)
-collection.remove(2)
-print(collection)
-collection.pop()
-print(collection)
+# collection.add(5)
+# print(collection)
+# collection.remove(2)
+# print(collection)
+# collection.pop()
+# print(collection)
+
+
+
+# set1={1,2,3}
+# set2={3,4,5}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+# print(set1.difference(set2))
+# print(set1.symmetric_difference(set2))
+
+
+#practice question 1
+
+table:
