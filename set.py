c89e22fcec4ec2be001af23f07f91d0e141b292c
Arunava Chakraborty <144585145+Arunava-chakrabor@users.noreply.github.com>
2026-02-20 23:21:54 +0530

    sets

---
+++ set.py
@@ -0,0 +1,11 @@
+collection={1,2,3,4}
+print(collection)
+print(type(collection))
+print(len(collection))
+
+collection.add(5)
+print(collection)
+collection.remove(2)
+print(collection)
+collection.pop()
+print(collection)
