7d8a8cb3efd9d44193c0cc7775caa386ebf44bc6
Arunava Chakraborty <144585145+Arunava-chakrabor@users.noreply.github.com>
2026-03-02 01:51:29 +0530

    recursion

--- recursion.py
+++ recursion.py
@@ -13,3 +13,17 @@ def fact(n):
         return 1
     return fact(n-1) * n
 print(fact(5))
+
+
+
+
+#write a recursive function to find the sum of first n natural numbers
+
+def  calc_sum(n):
+    if(n==0):
+        return 0
+    print(n)
+    return calc_sum(n-1) + n
+djfb
+jfdbsv
+print(calc_sum(5))
