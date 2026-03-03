4ed2c5ab98410304d742b659effd470498424f07
Arunava Chakraborty <144585145+Arunava-chakrabor@users.noreply.github.com>
2026-03-03 21:27:56 +0530

    recursion

--- recursion.py
+++ recursion.py
@@ -24,20 +24,7 @@ def  calc_sum(n):
         return 0
     print(n)
     return calc_sum(n-1) + n
-djfb
-jfdbsv
-print(calc_sum(5))
-def
-    fib(n):
-        if(n==0 or n==1):
-            return n
-        return fib(n-1) + fib(n-2)
-dh
-hfnhg 
-fdgb
-
-print(fib(10))
-
-set1={1,2,3,4,5}
-set2={4,5,6,7,8}
-print(set1.union(set2))
+def calc_sum(n):
+    if(n==0):
+        return 0
+    return calc_sum(n-1) + n
